85351787376af0cafcb57a42c37960fe118a9fd4
rleahy22 <ryanleahy90@gmail.com>
2015-08-06 22:43:21 -0400

    added changes to dist

--- dist/angularJsonEdit.js
+++ dist/angularJsonEdit.js
@@ -11,7 +11,7 @@
       '<div class="json-form-div">' +
         '<form name="jsonEditorForm" ng-submit="" role="form">' +
           '<label class="json-brackets">{</label>' +
-          '<div ng-repeat="(key, value) in config track by key" ng-init="parent = config; child = value" class="json-form" ng-include="\'../src/jsonNestTemplate.view.html\'">' +
+          '<div ng-repeat="(key, value) in config track by key" ng-init="parent = config; child = value" class="json-form" compile="nest"' +
           '</div>' +
           '<div json-editor-add-property class="json-new-property" object="config" newProperty="{}">' +
           '</div>' +
@@ -37,6 +37,24 @@
       scope.getInputType   = getInputType;
       scope.isArray        = isArray;
       scope.isNested       = isNested;
+
+      scope.nest = '<button class="json-delete json-button" ng-click="deleteProperty(key, parent)">&times;</button>' +
+        '<label ng-hide="isArray(parent)" class="json-form-element">' +
+          '{{key}}:' +
+          '<span ng-show="isNested(value) && isArray(value)">[</span>' +
+          '<span ng-show="isNested(value) && !isArray(value)">{</span>' +
+        '</label>' +
+        '<label ng-show="isNested(value) && !isArray(value)  && isArray(parent)" class="json-form-element">{</label>' +
+        '<div ng-if="isNested(value)" class="nested-json">' +
+          '<div ng-repeat="(key, value) in parent[key] track by key" ng-init="parent = child; child = value" class="json-form-row" compile="nest">' +
+          '</div>' +
+        '</div>' +
+        '<div ng-if="!isNested(value)" class="json-form-element">' +
+          '<input type="{{getInputType(value)}}" name="{{key}}" ng-model="parent[key]" class="json-input" required>' +
+        '</div>' +
+        '<div json-editor-add-property class="json-new-property" object="value" newProperty="{}" class="" ng-show="isNested(value)">' +
+        '</div>' +
+        '<label ng-show="isNested(value)" class="json-form-element">{{isArray(value) ? \']\' : \'}\'}}</label>';
 
       function deleteProperty(key, object) {
         if (scope.isArray(object)) {
