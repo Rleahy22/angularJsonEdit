d1ff04e19db5581d035dbccdd625bd3cc5c9e311
rleahy22 <ryanleahy90@gmail.com>
2015-08-08 16:54:23 -0400

    added code folding arrows

--- dist/angularJsonEdit.js
+++ dist/angularJsonEdit.js
@@ -31,30 +31,42 @@
     return directive;
 
     function link(scope) {
+      scope.collapse       = collapse;
+      scope.collapsed      = [];
       scope.deleteProperty = deleteProperty;
+      scope.expand         = expand;
       scope.getInputType   = getInputType;
       scope.isArray        = isArray;
+      scope.isCollapsed    = isCollapsed;
       scope.isNested       = isNested;
 
       scope.nest = '<button class="json-delete json-button" ng-click="deleteProperty(key, parent)">&times;</button>' +
-        '<div class="label-wrapper">' +
+        '<div class="label-wrapper" ng-class="{\'padded-row\': !isNested(value)}">' +
+          '<i ng-show="isNested(value) && isCollapsed(key, parent)" class="json-arrow" ng-click="expand(key, parent)">&#8658;</i>' +
+          '<i ng-show="isNested(value) && !isCollapsed(key, parent)" class="json-arrow" ng-click="collapse(key, parent)">&#8659;</i>' +
           '<label ng-hide="isArray(parent)" class="json-form-element">' +
             '<span class="key-span">{{key}}:</span>' +
-            '<span ng-show="isNested(value) && isArray(value)">[</span>' +
-            '<span ng-show="isNested(value) && !isArray(value)">{</span>' +
+            '<span ng-show="isNested(value) && isArray(value)"> [</span>' +
+            '<span ng-show="isNested(value) && !isArray(value)"> {</span>' +
           '</label>' +
           '<div ng-if="!isNested(value)" class="json-form-element">' +
             '<input type="{{getInputType(value)}}" name="{{key}}" ng-model="parent[key]" class="json-input" required>' +
           '</div>' +
         '</div>' +
         '<label ng-show="isNested(value) && !isArray(value)  && isArray(parent)" class="json-form-element">{</label>' +
-        '<div ng-if="isNested(value)" class="nested-json">' +
+        '<div ng-if="isNested(value)" ng-show="!isCollapsed(key, parent)" class="nested-json">' +
           '<div ng-repeat="(key, value) in parent[key] track by key" ng-init="parent = child; child = value" class="json-form-row" compile="nest">' +
           '</div>' +
+          '<div json-editor-add-property class="json-new-property padded-row" object="value" newProperty="{}" class="" ng-show="isNested(value)">' +
+          '</div>' +
         '</div>' +
-        '<div json-editor-add-property class="json-new-property" object="value" newProperty="{}" class="" ng-show="isNested(value)">' +
-        '</div>' +
-        '<label ng-show="isNested(value)" class="json-form-element">{{isArray(value) ? \']\' : \'}\'}}</label>';
+        '<label ng-show="isNested(value)" class="json-form-element padded-row">{{isArray(value) ? \']\' : \'}\'}}</label>';
+
+      function collapse(key, parent) {
+        var newObj = {}
+        newObj[key] = parent;
+        scope.collapsed.push(newObj);
+      }
 
       function deleteProperty(key, object) {
         if (scope.isArray(object)) {
@@ -62,6 +74,18 @@
         } else {
           delete object[key];
         }
+      }
+
+      function expand(key, parent) {
+        var check = {};
+        var result = false
+        check[key] = parent;
+
+        scope.collapsed.forEach(function(element, index) {
+          if (element[key] === check[key]) {
+            scope.collapsed.splice(index, 1);
+          }
+        });
       }
 
       function getInputType(value) {
@@ -74,6 +98,20 @@
 
       function isArray(value) {
         return Array.isArray(value);
+      }
+
+      function isCollapsed(key, parent) {
+        var check = {};
+        var result = false
+        check[key] = parent;
+
+        scope.collapsed.forEach(function(element) {
+          if (element[key] === check[key]) {
+            result = true;
+          }
+        });
+
+        return result;
       }
 
       function isNested(value) {
@@ -105,7 +143,7 @@
         '<button class="json-button" ng-click="addProperty()" ng-disabled="!newProperty.name || !newProperty.type">Add</button>' +
       '</div>' +
     '<div class="new-property-button-div" ng-show="!showForm">' +
-      '<button class="json-button" ng-click="showForm = true">&#43;</button>' +
+      '<button class="json-button padded-row" ng-click="showForm = true">&#43;</button>' +
     '</div>';
 
     var directive = {
